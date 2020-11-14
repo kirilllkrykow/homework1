421d332862e528635fdfa35bd99a1fd4aea0fee8
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-11-14 14:16:31 +0300

    Delete 2.2.py

--- 2.2.py
+++
@@ -1,8 +0,0 @@
-my_list = list(input("Введите список значений:"))
-n=0
-while n<len(my_list):
-...     my_list1=my_list[n:n+2]
-...     my_list1.reverse()
-...     my_list[n:n+2]=my_list1
-...     n=n+2
-... else: print ("список окончен")
