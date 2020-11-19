ba8045932c932ce60471122810ad9c2e0bafca92
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-11-19 21:07:07 +0300

    Delete 4.2.py

--- 4.2.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-my_list = [randint (0,100) for i in range (25)]
-list2 = [num for num in my_list if my_list[my_list.index(num)+1]<my_list[my_list.index(num)]]
-print (list2)
