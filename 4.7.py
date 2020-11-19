18a0a1bda45f7a6fcf88d2352bf0c3f51cd5e4bd
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-11-19 21:07:57 +0300

    Delete 4.7.py

--- 4.7.py
+++
@@ -1,9 +0,0 @@
-def fact(num):
-    n = 1
-    if num == 0:
-        yield f"{num}!=1"
-    for el in range(1, num+1):
-        n *=el
-        yield f"{el}!={n}"
-for el in fact(int(input("введите число:"))):
-    print(el)            
