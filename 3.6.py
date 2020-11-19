85809b1dc610d977c78fcce3ee59cb56d0d041ea
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-11-19 21:06:20 +0300

    Delete 3.6.py

--- 3.6.py
+++
@@ -1,8 +0,0 @@
-def int_func():
-...     for word in input("Введите строку из слов на латинице, разделенных пробелом:\n").split():
-...         chars = 0
-...         for char in word:
-...             if 97 <= ord(char) <= 122:
-...                 chars+=1
-...             print (word.title() if chars == len(word) else f"{word} - only small Latin letters")
-... int_func()
