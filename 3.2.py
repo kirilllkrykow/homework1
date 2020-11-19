fab63a7556b7c6e21a01fc6c49d143fc019c814d
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-11-19 21:05:30 +0300

    Delete 3.2.py

--- 3.2.py
+++
@@ -1,8 +0,0 @@
-def my_f(**kwargs):
-...     return kwargs
-... print(my_f(name = str(input("Введите ваше имя:")),\
-... surname = str(input("Введите вашу фамилию:")),\
-... year = int(input("Введите год рождения:")),\
-... city = str(input("Введите город проживания:")),\
-... email = str(input("Введите email:")),\
-... number = str(input("Введите номер телефона:"))))
