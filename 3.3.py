d6012e79078c53a844cdf3157e8906bb28a04e4f
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-11-19 21:05:38 +0300

    Delete 3.3.py

--- 3.3.py
+++
@@ -1,9 +0,0 @@
-def my_func():
-...     n=int(input("Введите первое число:"))
-...     m=int(input("Введите второе число:"))
-...     l=int(input("Введите третье число:"))
-...     my_list = [n,m,l]
-...     my_list.sort()
-...     result = f"{my_list[-1]+my_list[-2]}"
-...     return result
-... print (my_func())
