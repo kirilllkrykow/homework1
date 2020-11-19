054dc2fe516ecc06ce81522002f99c593fe5d238
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-11-19 21:05:14 +0300

    Delete 3.1.py

--- 3.1.py
+++
@@ -1,9 +0,0 @@
-def my_f():
-...     try:
-...         n= int(input("Введите первое число:"))
-...         m= int(input("Введите второе число:"))
-...     except ZeroDivisionError:
-...         print ("Делить на ноль нельзя!")
-...     result = f"{(n/m):.2f}"
-...     return result
-... print (my_f())
