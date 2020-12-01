61b2140a5268c1e66e6924e43c88f6de951eeb95
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-12-02 00:46:32 +0300

    Delete 1.6.py

--- 1.6.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Введите результат вашей пробежки в первый день:"))
-b = int(input("Введите желаемый результат:"))
-day=1
-while a<b:
-...     a=a*1.1
-...     day +=1
-... print(day, "Congratulations!")
