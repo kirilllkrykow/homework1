92567e9241c65dc9c05fe2f735bd18b2dc4f7fa7
kirilllkrykow <73797764+kirilllkrykow@users.noreply.github.com>
2020-12-02 00:46:14 +0300

    Delete 1.4.py

--- 1.4.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Введите любое целое положительное число:"))
-Max=0
-while n >= 10:
-...     b = n % 10
-...     if max < b:
-...         max=b
-...     n = n // 10
-... else: print (max)
